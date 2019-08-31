8cf7e8086c55967b4023910872d2622aec2ebd52
Logan-Shi <837898231@qq.com>
2019-09-01 02:17:16 +0800

    strategy done

--- base_controller/scripts/assigner.py
+++ base_controller/scripts/assigner.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import Pose, Quaternion, Point, Twist
 import actionlib
 import tf
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import math
 
 
 class assigner():
@@ -18,15 +19,21 @@ class assigner():
                             'SUCCEEDED', 'ABORTED', 'REJECTED', 
                             'PREEMPTING', 'RECALLING', 'RECALLED', 
                             'LOST')
-        self.car_states = ('LIGHT','TURN','STRAIGHT','PARK')
+        self.car_states = {'LIGHT'   : 0,
+                           'LEFT'    : 1,
+                           'RIGHT'   : 2,
+                           'EXITTURN': 3,
+                           'STRAIGHT': 4,
+                           'DYNAMIC' : 5,
+                           'PARK'    : 6}
         self.state = 0
         self.left_turn_pose = Pose(Point(9,2.3,0),Quaternion(0,0,0,1))
         self.right_turn_pose = Pose(Point(9,-2.3,0),Quaternion(0,0,0,1))
         self.exit_turn_pose = Pose(Point(13,0,0),Quaternion(0,0,0,1))
         self.straight_lane_pose = Pose(Point(13,4,0),Quaternion(0,0,1,0))
         self.park_request_pose = Pose(Point(5,4,0),Quaternion(0,0,1,0))
-        self.park_one_pose = Pose(Point(3,4,0),Quaternion(0,0,1,0))
-        self.park_two_pose = Pose(Point(3,2,0),Quaternion(0,0,1,0))
+        self.park_one_pose = Pose(Point(1,4,0),Quaternion(0,0,1,0))
+        self.park_two_pose = Pose(Point(1,2,0),Quaternion(0,0,1,0))
         self.pose = Pose()
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         self.map_base_link_tf_listener = tf.TransformListener()
@@ -43,17 +50,20 @@ class assigner():
         goal.target_pose.pose = pose
 
         self.client.send_goal(goal)
-        self.checkGoalStatus()
+        return self.checkGoalStatus()
             
     def checkGoalStatus(self):
         if not self.client.wait_for_result(rospy.Duration(300)):
             rospy.loginfo("Timed out to achieve goal!")
+            return 2
         else:
             state = self.client.get_state()
             if self.goal_states[state] == 'SUCCEEDED': 
                 rospy.loginfo("Goal reached!")
+                return 0
             else:
                 rospy.loginfo("Goal failed:" + str(self.goal_states[state]))
+                return 1
 
     def updatePose(self):
         self.map_base_link_tf_listener.waitForTransform('/base_link','/map',rospy.Time(), rospy.Duration(0.5))
@@ -68,27 +78,93 @@ class assigner():
         rospy.loginfo("pose = \n" + str(self.pose))
 
     def waitforLight(self):
-        while self.car_states[self.state] == 'LIGHT':
+        while self.state == self.car_states['LIGHT']:
             lightStatus = 1
             if lightStatus == 0:
                 rospy.loginfo('waiting for light signal...')
                 self.stop()
+                self.state = self.car_states['LIGHT']
             else :
                 rospy.loginfo('signal received.')
-                self.state = 1
                 if lightStatus == 1:
                     rospy.loginfo('turning right')
-                    self.sendGoal(self.right_turn_pose)
+                    self.state = self.car_states['RIGHT']
                 else:
                     rospy.loginfo('turning left')
-                    self.sendGoal(self.left_turn_pose)
-            return lightStatus
+                    self.state = self.car_states['LEFT']
+                
+    def turnRight(self):
+        while self.state == self.car_states['RIGHT']:
+            if not self.sendGoal(self.right_turn_pose):
+                self.state = self.car_states['EXITTURN']
+            else:
+                self.state = self.car_states['RIGHT']
+
+    def turnLeft(self):
+        while self.state == self.car_states['LEFT']:
+            if not self.sendGoal(self.left_turn_pose):
+                self.state = self.car_states['EXITTURN']
+            else:
+                self.state = self.car_states['LEFT']
+
+    def exitTurn(self):
+        while self.state == self.car_states['EXITTURN']:
+            rospy.loginfo('exiting turn')
+            if not self.sendGoal(self.exit_turn_pose):
+                self.state = self.car_states['STRAIGHT']
+            else:
+                self.state = self.car_states['EXITTURN']
 
     def goStraight(self):
-        pass
+        while self.state == self.car_states['STRAIGHT']:
+            self.client.cancel_goal()
+            if self.isArrived(self.straight_lane_pose):
+                self.state = self.car_states['DYNAMIC']
+            else:
+                self.state = self.car_states['STRAIGHT']
+
+    def isArrived(self,pose):
+        isArrived = 0
+        self.updatePose()
+        if self.abs(self.pose.position.x - pose.position.x) < 0.1 and \
+           self.abs(self.pose.position.y - pose.position.y) < 0.5:
+            isArrived = 1
+            rospy.loginfo('target reached!')
+        else:
+            rospy.loginfo('approaching target...')
+        return isArrived
+
+    def waitforDynamicObj(self):
+        while self.state == self.car_states['DYNAMIC']: 
+            rospy.loginfo('waiting for dynamic obj')
+            isFront = 0
+            if isFront:
+                rospy.loginfo('pedestrian detected, stopping...')
+                self.stop()
+                self.state = self.car_states['DYNAMIC']
+            else:
+                rospy.loginfo('passage way clear!')
+                self.state = self.car_states['PARK']
+
+    def abs(self,a):
+        if a>0:
+            return a
+        else:
+            return -a
 
     def park(self):
-        pass
+        while self.state == self.car_states['PARK']:
+            park = 1
+            if park == 1:
+                if not self.sendGoal(self.park_one_pose):
+                    rospy.loginfo('parked into spot one')
+                else:
+                    self.state == self.car_states['PARK']
+            else:
+                if not self.sendGoal(self.park_two_pose):
+                    rospy.loginfo('parked into spot two')
+                else:
+                    self.state == self.car_states['PARK']
         
     def _shutdown(self):
         self.client.cancel_goal()
@@ -96,16 +172,13 @@ class assigner():
 
     def spin(self):
         while not rospy.is_shutdown():
-            self.updatePose()
-            while self.waitforLight():
-                if self.car_states[self.state] == 'TURN':
-                    rospy.loginfo('exiting turn')
-                    self.sendGoal(self.exit_turn_pose)
-                    self.state = 2
-                else:
-                    self.state = 0
-                self.goStraight()
-                self.park()
+            self.waitforLight()
+            self.turnLeft()
+            self.turnRight()
+            self.exitTurn()
+            self.goStraight()
+            self.waitforDynamicObj()
+            self.park()
             self.rate.sleep()
 
     def stop(self):
