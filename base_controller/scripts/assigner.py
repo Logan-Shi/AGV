0b4a7b6835adcb8f00ca3c6c61e7b3a82ece319b
Logan-Shi <837898231@qq.com>
2019-09-01 00:00:10 +0800

    whole map added

--- base_controller/scripts/assigner.py
+++ base_controller/scripts/assigner.py
@@ -2,7 +2,7 @@
 # license removed for brevity
 
 import rospy
-from geometry_msgs.msg import Pose, Quaternion, Point
+from geometry_msgs.msg import Pose, Quaternion, Point, Twist
 import actionlib
 import tf
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
@@ -18,14 +18,23 @@ class assigner():
                             'SUCCEEDED', 'ABORTED', 'REJECTED', 
                             'PREEMPTING', 'RECALLING', 'RECALLED', 
                             'LOST')
+        self.car_states = ('LIGHT','TURN','STRAIGHT','PARK')
+        self.state = 0
+        self.left_turn_pose = Pose(Point(9,2.3,0),Quaternion(0,0,0,1))
+        self.right_turn_pose = Pose(Point(9,-2.3,0),Quaternion(0,0,0,1))
+        self.exit_turn_pose = Pose(Point(13,0,0),Quaternion(0,0,0,1))
+        self.straight_lane_pose = Pose(Point(13,4,0),Quaternion(0,0,1,0))
+        self.park_request_pose = Pose(Point(5,4,0),Quaternion(0,0,1,0))
+        self.park_one_pose = Pose(Point(3,4,0),Quaternion(0,0,1,0))
+        self.park_two_pose = Pose(Point(3,2,0),Quaternion(0,0,1,0))
         self.pose = Pose()
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         self.map_base_link_tf_listener = tf.TransformListener()
+        self.velPub = rospy.Publisher("/cmd_vel_mux/input/assigner",\
+                Twist, queue_size =1 )
         rospy.sleep(1)
 
     def sendGoal(self,pose):
-        rospy.loginfo("send new goal:\n" + str(pose))
-        
         self.client.wait_for_server()
 
         goal = MoveBaseGoal()
@@ -58,22 +67,52 @@ class assigner():
         self.pose.orientation.w = rot[3]
         rospy.loginfo("pose = \n" + str(self.pose))
 
-    def parser(self,msg):
-        goal_pose = Pose(Point(0,0,0),Quaternion(0,0,0,1))
-        end_pose = Pose(Point(-1.7,-3.9,0),Quaternion(0,0,1,0))
-        if (self.pose.position.x - end_pose.position.x < 0.1) and (self.pose.position.y - end_pose.position.y < 0.1):
-            self.sendGoal(goal_pose)
-        else:
-            pass
+    def waitforLight(self):
+        while self.car_states[self.state] == 'LIGHT':
+            lightStatus = 1
+            if lightStatus == 0:
+                rospy.loginfo('waiting for light signal...')
+                self.stop()
+            else :
+                rospy.loginfo('signal received.')
+                self.state = 1
+                if lightStatus == 1:
+                    rospy.loginfo('turning right')
+                    self.sendGoal(self.right_turn_pose)
+                else:
+                    rospy.loginfo('turning left')
+                    self.sendGoal(self.left_turn_pose)
+            return lightStatus
+
+    def goStraight(self):
+        pass
+
+    def park(self):
+        pass
         
     def _shutdown(self):
         self.client.cancel_goal()
+        self.stop()
 
     def spin(self):
         while not rospy.is_shutdown():
             self.updatePose()
-            self.parser(0)
+            while self.waitforLight():
+                if self.car_states[self.state] == 'TURN':
+                    rospy.loginfo('exiting turn')
+                    self.sendGoal(self.exit_turn_pose)
+                    self.state = 2
+                else:
+                    self.state = 0
+                self.goStraight()
+                self.park()
             self.rate.sleep()
+
+    def stop(self):
+        drive_msg = Twist()
+        drive_msg.linear.x = 0.0
+        drive_msg.angular.z = 0.0
+        self.velPub.publish(drive_msg)
 
 if __name__ == '__main__':
     try:
